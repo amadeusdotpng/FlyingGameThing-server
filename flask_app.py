28c9e192f6c72d79cdd826ef4098d82a1589347f
Amadeus8182 <amadeus8182@gmail.com>
2024-05-09 15:41:40 -0500

    big commit with a bad commit message... stuff works now though...

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,5 @@
-from math import sin, cos
-from math import radians as rad
 from flask import Flask, jsonify, Response, request
 from random import choice
-import numpy as np
 import uuid
 
 # Godly resource: 
@@ -20,7 +17,7 @@ app = Flask(__name__)
         'timestamp': timestamp,
         'pos': {'x': x, 'y': y, 'z': z},
         'vel': {'x': x, 'y': y, 'z': z}, 
-        'dir': {'x': x, 'y': y, 'z': z},
+        'acc': {'x': x, 'y': y, 'z': z}, 
         'win_time': win_time,
     }
 }
@@ -61,53 +58,25 @@ FWD_SPEED: float = 120
 BCK_SPEED: float = -25
 SPEED = [BCK_SPEED, IDLE_SPEED, FWD_SPEED]
 
-# default Godot _physics_process() delta value
-DELTA: float = 1/60
-@app.route('/set_data')
+# default timer
+DELTA = 1/60 * 0.067
+@app.route('/set_data', methods=['POST'])
 def set_data():
-    # We're only going to send ACTIONS to the server, i.e. we send 
-    # only things we WANT to do. We never want the client to have
-    # the authority of telling the server what the game state should
-    # look like, otherwise we easily get cheaters. In our case, we
-    # send the client's current acceleration and rotation movement.
-    # However, because it takes a lot of math and I'm lazy, we're
-    # going to be sending the direction the plane is facing instead
-    # of calculating it.
-    #
-    # The server then calculates what the next game state 
-    # should be and sends it back to the client.
-    if ('uuid' not in request.args or
-        'acc' not in request.args or
-        'dir_x' not in request.args or
-        'dir_y' not in request.args or
-        'dir_z' not in request.args or
-        'timestamp' not in request.args):
-        return Response(status=400)
+    data = request.get_json()
+
+    if (data is None or 'timestamp' not in data or 'uuid' not in data or
+        'pos' not in data or 'vel' not in data or 'acc' not in data):
+
+        return Response(response='Bad Data', status=400)
+
+    player = players[data['uuid']]
+    player['pos'] = data['pos']
+    player['vel'] = data['vel']
+    player['acc'] = data['acc']
+    player['timestamp'] = data['timestamp']
 
 
-    player = players[request['uuid']]
-
-    try:
-        if int(request['timestamp']) < int(player['timestamp']):
-            return Response(status=204)
-
-        acc = int(request['acc'])
-        dir_x = float(request['dir_x'])
-        dir_y = float(request['dir_y'])
-        dir_z = float(request['dir_z'])
-
-        player['dir'] = {'x': dir_x, 'y': dir_y, 'z': dir_z}
-        player['vel']['x'] += dir_x * SPEED[acc] * DELTA
-        player['vel']['y'] += dir_y * SPEED[acc] * DELTA
-        player['vel']['z'] += dir_z * SPEED[acc] * DELTA
-
-        player['pos']['x'] += player['vel']['x'] * DELTA
-        player['pos']['y'] += player['vel']['y'] * DELTA
-        player['pos']['z'] += player['vel']['z'] * DELTA
-
-        return Response(status=204)
-    except:
-        return Response(status=400)
+    return Response(response='Success', status=200)
 
 
 @app.route('/connect')
@@ -117,10 +86,14 @@ def connect():
     players[player_uuid]['username'] = choice(names)
     players[player_uuid]['pos'] = {'x': 0, 'y': 0, 'z': 0}
     players[player_uuid]['vel'] = {'x': 0, 'y': 0, 'z': 0}
-    players[player_uuid]['dir'] = {'x': 0, 'y': 0, 'z': 0}
-    return Response(response=player_uuid, status=201)
+    players[player_uuid]['acc'] = {'x': 0, 'y': 0, 'z': 0}
+    players[player_uuid]['timestamp'] = 0
+
+    resp = jsonify({'uuid': player_uuid})
+    resp.status_code = 201
+    return resp
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
