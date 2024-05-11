d86eb187ba3ce74b246cbae6724823b5a8cbdafb
Amadeus8182 <amadeus8182@gmail.com>
2024-05-11 07:38:28 -0500

    Revert "oops"
    This reverts commit dee28d982c72f8585283680991f0b10a8bc7cc0f.
    
    we're giving up on tweening...we're gonna use a simpler solution

--- flask_app.py
+++ flask_app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
         'username': username,
         'timestamp': timestamp,
         'pos': {'x': x, 'y': y, 'z': z},
-        'rot': {'x': x, 'y': y, 'z': z}, 
+        'vel': {'x': x, 'y': y, 'z': z}, 
+        'acc': {'x': x, 'y': y, 'z': z}, 
         'win_time': win_time,
     }
 }
@@ -51,15 +52,17 @@ def set_data():
     data = request.get_json()
 
     if (data is None or 'timestamp' not in data or 'uuid' not in data or
-        'rot' not in data or 'pos' not in data):
+        'rot' not in data or 'pos' not in data or 'vel' not in data or 'acc' not in data):
         return Response(response='Bad Data', status=400)
 
     if data['uuid'] not in players:
-        create_player()
+        return redirect('/connect')
 
     player = players[data['uuid']]
     player['rot'] = data['rot']
     player['pos'] = data['pos']
+    player['vel'] = data['vel']
+    player['acc'] = data['acc']
     player['timestamp'] = data['timestamp']
 
     return Response(response='Success', status=200)
@@ -67,21 +70,18 @@ def set_data():
 
 @app.route('/connect')
 def connect():
-    player_uuid = create_player()
-    resp = jsonify({'uuid': player_uuid})
-    resp.status_code = 201
-    return resp
-
-def create_player():
     player_uuid = str(uuid.uuid4())
     players[player_uuid] = {}
     players[player_uuid]['username'] = choice(names)
     players[player_uuid]['rot'] = {'x': 0, 'y': 0, 'z': 0}
     players[player_uuid]['pos'] = {'x': 0, 'y': 0, 'z': 0}
+    players[player_uuid]['vel'] = {'x': 0, 'y': 0, 'z': 0}
+    players[player_uuid]['acc'] = {'x': 0, 'y': 0, 'z': 0}
     players[player_uuid]['timestamp'] = 0
 
-    return player_uuid
-
+    resp = jsonify({'uuid': player_uuid})
+    resp.status_code = 201
+    return resp
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)instance.rotation_degrees = rot
